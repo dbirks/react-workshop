bc1b5abe6f7ace30a04b9e2c0100537cd41aa02d
David Birks <david@birks.dev>
2021-08-17 12:44:36 -0400

    Add App.tsx

---
+++ src/index.tsx
@@ -0,0 +1,13 @@
+import { render } from 'react-dom';
+import { App } from "./App";
+
+// my first react component
+
+// HTML            vs              JSX
+// class                           className
+// for                             htmlFor
+// inline styles are strings       inline styles are objects, numbers = px
+
+render(<App />, document.getElementById("root"));
+
+
