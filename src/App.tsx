c8accf3d4234aa75c6e18415f21c74494292cfd4
David Birks <david@birks.dev>
2021-08-17 12:50:44 -0400

    Add alternate ways to do functions

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,17 @@
+// Function declaration
+// Preferred, to be able to see at a glance that it's a function.
+// Function declarations are also hoisted to the top of the file.
 export function App() {
     return <h1>Pantry Manager</h1>
 }
+
+// // Function expression
+// const App2 = function () {
+//     return <h1>Pantry Manager</h1>
+// }
+
+// // Arrow function
+// const App3 = () => <h1>Pantry Manager</h1>
 
 
 
