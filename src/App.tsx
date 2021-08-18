d2b8ab6291798e4cef9a901d404f3b8ddfe066aa
David Birks <david@birks.dev>
2021-08-18 10:54:01 -0400

    Start form

--- src/App.tsx
+++ src/App.tsx
@@ -3,73 +3,79 @@ import { useEffect } from "react";
 import { deleteFood, getFoods } from "./api/foodsApi";
 
 export type Food = {
-    id: number;
-    name: string;
-    quantity: number;
-    minQuantity: number;
-    type: string;
-}
+  id: number;
+  name: string;
+  quantity: number;
+  minQuantity: number;
+  type: string;
+};
 
 export function App() {
-    const [foods, setFoods] = useState<Food[]>([]);
+  const [foods, setFoods] = useState<Food[]>([]);
 
-    useEffect(() => {
-        async function callGetFoods() {
-            // Using underscore to prevent naming conflict
-            const _foods = await getFoods();
-            setFoods(_foods);
-        }
-        callGetFoods();
-        // Using empty array for useEffect since we only want it to run once
-    }, [])
+  useEffect(() => {
+    async function callGetFoods() {
+      // Using underscore to prevent naming conflict
+      const _foods = await getFoods();
+      setFoods(_foods);
+    }
+    callGetFoods();
+    // Using empty array for useEffect since we only want it to run once
+  }, []);
 
-    return (
-        <>
-            <h1>Pantry Manager</h1>
-            <table>
-                <thead>
-                    <tr>
-                        <th>
-                            Name
-                        </th>
-                        <th>
-                            Quantity
-                        </th>
-                        <th>
-                            Min Quantity
-                        </th>
-                        <th>
-                            Type
-                        </th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {/* Exercise 3: Add a delete button next to the name. When clicked, alert('clicked').} */}
-                    {foods.map((food) => (
-                        <tr key={food.name}>
-                            <td>
-                                <button
-                                    onClick={async () => {
-                                        await deleteFood(food.id);
-                                        // Return a new array with the id that was just deleted
-                                        const newFoods = foods.filter((f) => f.id !== food.id);
-                                        // remove the deleted food from state
-                                        setFoods(newFoods);
-                                    }}
-                                >
-                                    Delete
-                                </button>
-                            </td>
-                            <td>{food.name}</td>
-                            <td>{food.quantity}</td>
-                            <td>{food.minQuantity}</td>
-                            <td>{food.type}</td>
-                        </tr>
-                    ))}
-                </tbody>
-            </table>
-        </>
-    )
+  return (
+    <>
+      <h1>Pantry Manager</h1>
+
+      <form action="">
+        <div>
+          <label>Name</label>
+          <br />
+          <input type="text" />
+        </div>
+        <div>
+          <label>Name</label>
+          <br />
+          <input type="text" />
+        </div>
+      </form>
+
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Quantity</th>
+            <th>Min Quantity</th>
+            <th>Type</th>
+          </tr>
+        </thead>
+        <tbody>
+          {/* Exercise 3: Add a delete button next to the name. When clicked, alert('clicked').} */}
+          {foods.map((food) => (
+            <tr key={food.name}>
+              <td>
+                <button
+                  onClick={async () => {
+                    await deleteFood(food.id);
+                    // Return a new array with the id that was just deleted
+                    const newFoods = foods.filter((f) => f.id !== food.id);
+                    // remove the deleted food from state
+                    setFoods(newFoods);
+                  }}
+                >
+                  Delete
+                </button>
+              </td>
+              <td>{food.name}</td>
+              <td>{food.quantity}</td>
+              <td>{food.minQuantity}</td>
+              <td>{food.type}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </>
+  );
 }
 
 // // Function declaration
@@ -86,7 +92,3 @@ export function App() {
 //
 // // Arrow function
 // const App3 = () => <h1>Pantry Manager</h1>
-
-
-
-
