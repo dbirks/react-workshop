68af7e0381e943cb1a6752bf7e3a60c75b485577
David Birks <david@birks.dev>
2021-08-19 14:36:00 -0400

    partial add of useContext

--- src/App.tsx
+++ src/App.tsx
@@ -3,28 +3,37 @@ import { BrowserRouter, Route } from "react-router-dom";
 import { About } from "./About";
 import { FoodForm } from "./FoodForm";
 import { Nav } from "./shared/Nav";
-import { QueryClient, QueryClientProvider } from "react-query";
+import { UserContextProvider, UserContextType } from "./UserContext";
+
+const user: UserContextType = {
+  email: "c@h.com",
+  name: "Cory",
+  role: "admin",
+  token: "1234",
+};
 
 export function App() {
   return (
-    <BrowserRouter>
-      <Nav />
+    <UserContextProvider value={user}>
+      <BrowserRouter>
+        <Nav />
 
-      <Route path="/about">
-        <About />
-      </Route>
+        <Route path="/about">
+          <About />
+        </Route>
 
-      <Route path="/food" exact>
-        <FoodForm />
-      </Route>
+        <Route path="/food" exact>
+          <FoodForm />
+        </Route>
 
-      <Route path="/food/:foodId">
-        <FoodForm />
-      </Route>
+        <Route path="/food/:foodId">
+          <FoodForm />
+        </Route>
 
-      <Route path="/" exact>
-        <Home />
-      </Route>
-    </BrowserRouter>
+        <Route path="/" exact>
+          <Home />
+        </Route>
+      </BrowserRouter>
+    </UserContextProvider>
   );
 }
--- src/shared/Nav.tsx
+++ src/shared/Nav.tsx
@@ -1,14 +1,22 @@
 import { Link } from "react-router-dom";
+import { useUserContext } from "../UserContext";
 
 export function Nav() {
+  const { name} = useUserContext();
+
   return (
     <nav>
+      <p>Hello {name}!</p>
       <ul className="nav">
         <li className="nav-item">
-          <Link className="nav-link" to="/">Home</Link>
+          <Link className="nav-link" to="/">
+            Home
+          </Link>
         </li>
         <li className="nav-item">
-          <Link className="nav-link" to="/about">About</Link>
+          <Link className="nav-link" to="/about">
+            About
+          </Link>
         </li>
       </ul>
     </nav>
